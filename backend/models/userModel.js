67ebdf440cbfadd6a0ec985e95ab33105fb6592b
Yeming Hu <87739782+jonney2021@users.noreply.github.com>
2023-11-04 23:02:32 -0400

    Backend Authentication

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
-import { timestamp } from "rxjs";
+import bcrypt from "bcryptjs";
 
 const userSchema = new mongoose.Schema(
   {
@@ -24,6 +24,19 @@ const userSchema = new mongoose.Schema(
   },
   { timestamp: true }
 );
+
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    next();
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
 
 const User = mongoose.model("User", userSchema);
 
