83e4a2bdf7c56a901f9a33e009f9b589f9b380df
Yeming Hu <yeminghuhym@gmail.com>
2023-11-26 11:39:35 -0500

    updated the products displaying

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -33,11 +33,14 @@ const HomeScreen = () => {
         <>
           <h1>Latest Products</h1>
           <Row>
-            {data.products.map((product) => (
-              <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-                <Product product={product} />
-              </Col>
-            ))}
+            {data.products
+              .slice()
+              .reverse() // Reverse the order of the products
+              .map((product) => (
+                <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                  <Product product={product} />
+                </Col>
+              ))}
           </Row>
           <Paginate
             pages={data.pages}
