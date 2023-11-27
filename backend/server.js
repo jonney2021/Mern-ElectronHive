1db65b95a9c82eba828c3cb20dfde30be5342fad
Yeming Hu <yeminghuhym@gmail.com>
2023-11-26 20:24:52 -0500

    update package.json and server.js

--- backend/server.js
+++ backend/server.js
@@ -143,34 +143,28 @@ app.get("/api/config/paypal", (req, res) =>
   res.send({ clientId: process.env.PAYPAL_CLIENT_ID })
 );
 
-const __dirname = path.resolve(); // Set __dirname to current directory
-app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
-
-app.use(notFound);
-app.use(errorHandler);
-
 const port = process.env.PORT || 5000;
 
 if (process.env.NODE_ENV === "production") {
+  const __dirname = path.resolve(); // Set __dirname to current directory
+  app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
   // set static folder
   app.use(express.static(path.join(__dirname, "/frontend/build")));
 
-  // Log middleware
-  app.use((req, res, next) => {
-    console.log("Middleware Log:", req.url);
-    next();
-  });
-
   // any route that is not api will be redirected
-  app.get("*", (req, res) => {
-    console.log("Wildcard route hit:", req.url);
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
-  });
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  );
 } else {
+  const __dirname = path.resolve();
+  app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
   app.get("/", (req, res) => {
-    res.send("API is running...");
+    res.send("API is running....");
   });
 }
+
+app.use(notFound);
+app.use(errorHandler);
 
 httpServer.listen(port, () => {
   console.log(`Server running on port ${port}`);
